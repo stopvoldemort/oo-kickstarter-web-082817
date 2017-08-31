3a36bd81f472f6ea5f9f24f7aa8b194d808561f8
stopvoldemort <david.squires1@gmail.com>
2017-08-31 18:26:51 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  attr_accessor :backers
+  attr_reader :title
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+
+end
